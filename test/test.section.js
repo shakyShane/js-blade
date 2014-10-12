ea4b3f16516e02199993272390d1cbd3526cd2a7
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-12 15:47:50 +0100

    add debugger

--- test/test.section.js
+++ test/test.section.js
@@ -2,22 +2,32 @@ var multi   = require("multiline");
 var blade   = require("../lib/blade").compile;
 var assert  = require("chai").assert;
 
+function debugOutput (string) {
+    var count = 0;
+    var out = string.replace(/\n/g, function () {
+        count += 1;
+        return "\n~~~"+count+":   "
+    });
+    console.log(out);
+}
+
 describe("@section", function(){
     it("can save content & place in @yield", function(){
         var template = multi.stripIndent(function () {/*
         @section("shane")
-        <p>Hi there from the first section
-            <span>Hello</span>
+        <p>This is within a section
+            <span>And this is a nested span</span>
         </p>
         @stop
 
         <div>Hi there</div>
         <header>
-            <h1>Hey you!</h1>
+            <h1>Page title</h1>
             @yield("shane")
         </header>
         */});
         var out = blade(template);
+        debugOutput(out);
         assert.notInclude(out, "@section(\"shane\")");
     });
 });
