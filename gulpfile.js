27c533dfa3c723afac7298ba8a75ce32b2049ba2
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-12 21:46:36 +0100

    Add watcher for style linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp        = require("gulp");
 var jshint      = require("gulp-jshint");
 var jscs        = require("gulp-jscs");
+var gutil       = require("gulp-util");
+var Multistream = require('multistream');
+
+var src  = [
+    "lib/*.js",
+    "!lib/parser.js"
+];
 
 gulp.task("lint", function () {
-    gulp.src([
-        "lib/*.js",
-        "!lib/parser.js"
-    ])
+    gulp.src(src)
         .pipe(jscs(".jscs.json"))
         .pipe(jshint())
         .pipe(jshint.reporter("default"))
         .pipe(jshint.reporter("fail"));
+});
+
+gulp.task("lint-watch", function () {
+    gulp.src(src)
+        .pipe(jscs(".jscs.json")).on("error", function (err) {
+            gutil.beep();
+            console.log(err.message);
+        });
+});
+
+gulp.task("watch", function () {
+    gulp.watch(src, ["lint-watch"]);
 });
