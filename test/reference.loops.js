8a79b3b46740354e80939e09e88375ad8729085b
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-27 07:16:07 +0000

    Added spec for loops

--- test/reference.loops.js
+++ test/reference.loops.js
@@ -1,58 +1,73 @@
-//var blade   = require("../lib/blade");
-//var compile = blade.compile;
-//var reset   = blade.reset;
-//var context = blade.context;
-//var dlog    = require("d-logger");
-//
-//var multi   = require("multiline");
-//var assert  = require("chai").assert;
-//
-//describe("Reference Section lookup {#name}", function(){
-//    afterEach(function () {
-//        reset();
-//    })
-//    it("should render inside a ref section loop", function(){
-//        var input = multi(function () {/*
-//Before
-//{name} - {job}
-//After
-//*/});
-//        var expected = "Before\nshane - dev\nAfter";
-//        var actual   = compile(input, {
-//            name: "shane",
-//            "job": "dev"
-//        });
-//        assert.equal(actual, expected);
-//    });
-//    it("should render inside a ref section loop", function(){
-//        var input = multi(function () {/*
-//Before
-//{greeting}
-//{#names}{.}{/names}
-//After
-//*/});
-//        var expected = "Before\nHello\nShaneOsbourne\nAfter";
-//        var actual   = compile(input, {
-//            names: ["Shane", "Osbourne"],
-//            greeting: "Hello"
-//        });
-//        dlog(actual);
-//        assert.equal(actual, expected);
-//    });
-//    it("should render inside a ref section loop", function(){
-//        var input = multi(function () {/*
-//Before
-//{#names}{first} - {last}{/names}
-//After
-//*/});
-//        var expected = "Before\nShane - Osbourne\nAfter";
-//        var actual   = compile(input, {
-//            names: [{
-//                first: "Shane",
-//                last: "Osbourne"
-//            }]
-//        });
-//        dlog(actual);
-//        assert.equal(actual, expected);
-//    });
-//});
+var blade   = require("../lib/blade");
+var compile = blade.compile;
+var reset   = blade.reset;
+var context = blade.context;
+var dlog    = require("d-logger");
+
+var multi   = require("multiline");
+var assert  = require("chai").assert;
+
+describe("Reference Section lookup {#name}", function(){
+    afterEach(function () {
+        reset();
+    })
+    it("should render inside a ref section loop", function(){
+        var input = multi(function () {/*
+Before
+{name} - {job}
+After
+*/});
+        var expected = "Before\nshane - dev\nAfter";
+        var actual   = compile(input, {
+            name: "shane",
+            "job": "dev"
+        });
+        assert.equal(actual, expected);
+    });
+    it("should render inside a ref section loop", function(){
+        var input = multi(function () {/*
+Before
+{greeting}
+{#names}{.}{/names}
+After
+*/});
+        var expected = "Before\nHello\nShaneOsbourne\nAfter";
+        var actual   = compile(input, {
+            names: ["Shane", "Osbourne"],
+            greeting: "Hello"
+        });
+        assert.equal(actual, expected);
+    });
+    it("should render inside a ref section loop", function(){
+        var input = multi(function () {/*
+Before
+{greeting} {#names}{first} {last}{/names}
+After
+*/});
+        var expected = "Before\nWelcome Shane Osbourne\nAfter";
+        var actual   = compile(input, {
+            names: [{
+                first: "Shane",
+                last: "Osbourne"
+            }],
+            greeting: "Welcome"
+        });
+        assert.equal(actual, expected);
+    });
+    it("should render inside a ref section loop", function(){
+        var input = multi(function () {/*
+Before
+{greeting} {names[0].first} {names[0].last}
+After
+*/});
+        var expected = "Before\nWelcome Shane Osbourne\nAfter";
+        var actual   = compile(input, {
+            names: [{
+                first: "Shane",
+                last: "Osbourne"
+            }],
+            greeting: "Welcome"
+        });
+        assert.equal(actual, expected);
+    });
+});
